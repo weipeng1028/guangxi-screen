d9b7e4182cda5d3465090e0acf274bc928f3a696
mc199645 <495750028@qq.com>
2020-06-22 10:10:08 +0800

    6/22/up

--- src/api/api.js
+++ src/api/api.js
@@ -5,30 +5,30 @@ export default {
   // upload: 'http://192.168.10.68:9998/product/media/common/file/upload', // POST 上传
   // download: 'http://47.93.1.52/product/media/common/file/download', // GET 文件下载
   // download: 'http://192.168.10.68:9998/product/media/common/file/download', // GET 文件下载
-  regions: '/bigscreen/regions', // GET 北京各区微信微博行为数据展示接口
-  channelsDayReadSum: '/bigscreen/channelsDayReadSum', // GET 大屏中下方各渠道日阅读总量
-  DistrictRanking: '/bigscreen/DistrictRanking', // GET 区排行榜
-  displayExcellentWorks: '/bigscreen/displayExcellentWorks', // GET 优秀作品展示
-  totalPublish: '/bigscreen/totalPublish', // GET 总文章发布数
-  hotArticles: '/bigscreen/hotArticles', // GET 热门文章排行
-  douyin_redirect: '/bigscreen/douyin_redirect', // GET 热门文章排行
-  channelsLatestReadNum: '/bigscreen/channelsLatestReadNum', // GET 热门文章排行
-  fans: '/bigscreen/fans', // GET 微信微博粉丝关注数
-  cityBehavioralData: '/bigscreen/cityBehavioralData', // GET 地区数据
-  cityHotArticles: '/bigscreen/cityHotArticles', // GET 市级别各渠道热门文章
-  excellentArticles: '/bigscreen/excellentArticles', // 优秀文章图片
-  monthReadDifference: '/bigscreen/monthReadDifference', // 热门文章趋势
+  regions: '/regions', // GET 北京各区微信微博行为数据展示接口
+  channelsDayReadSum: '/channelsDayReadSum', // GET 大屏中下方各渠道日阅读总量
+  DistrictRanking: '/DistrictRanking', // GET 区排行榜
+  displayExcellentWorks: '/displayExcellentWorks', // GET 优秀作品展示
+  totalPublish: '/totalPublish', // GET 总文章发布数
+  hotArticles: '/hotArticles', // GET 热门文章排行
+  douyin_redirect: '/douyin_redirect', // GET 热门文章排行
+  channelsLatestReadNum: '/channelsLatestReadNum', // GET 热门文章排行
+  fans: '/fans', // GET 微信微博粉丝关注数
+  cityBehavioralData: '/cityBehavioralData', // GET 地区数据
+  cityHotArticles: '/cityHotArticles', // GET 市级别各渠道热门文章
+  excellentArticles: '/excellentArticles', // 优秀文章图片
+  monthReadDifference: '/monthReadDifference', // 热门文章趋势
   // 传播分析
-  wxInformation: '/bigscreen/wxInformation', // 获取微信账号信息
-  wbInformation: '/bigscreen/wbInformation', // 获取微博账号信息
-  wxPublishArticles: '/bigscreen/wxPublishArticles', // 发布文章 传播趋势图
-  wxUserInteraction: '/bigscreen/wxUserInteraction', // 用户互动 传播趋势图
-  wbPublishArticles: '/bigscreen/wbPublishArticles', // 发布文章 传播趋势图
-  wbUserInteraction: '/bigscreen/wbUserInteraction', // 用户互动 传播趋势图
-  wxReproducedByOfficialTop10: '/bigscreen/wxReproducedByOfficialTop10', // 被公众号转载Top10
-  communicationPowerRanking: '/bigscreen/communicationPowerRanking', // 传播力排行榜
-  popularArticlesTop10: '/bigscreen/popularArticlesTop10', // 微博热门文章
-  wechatCommunicationForceScore: '/bigscreen/wechatCommunicationForceScore', // 微信传播力分数
-  weiboCommunicationForceScore: '/bigscreen/weiboCommunicationForceScore', // 微博传播力分数
-  PropagationPath: '/bigscreen/bigscreenPopularArticlesPropagationPath' // 微博路径
+  wxInformation: '/wxInformation', // 获取微信账号信息
+  wbInformation: '/wbInformation', // 获取微博账号信息
+  wxPublishArticles: '/wxPublishArticles', // 发布文章 传播趋势图
+  wxUserInteraction: '/wxUserInteraction', // 用户互动 传播趋势图
+  wbPublishArticles: '/wbPublishArticles', // 发布文章 传播趋势图
+  wbUserInteraction: '/wbUserInteraction', // 用户互动 传播趋势图
+  wxReproducedByOfficialTop10: '/wxReproducedByOfficialTop10', // 被公众号转载Top10
+  communicationPowerRanking: '/communicationPowerRanking', // 传播力排行榜
+  popularArticlesTop10: '/popularArticlesTop10', // 微博热门文章
+  wechatCommunicationForceScore: '/wechatCommunicationForceScore', // 微信传播力分数
+  weiboCommunicationForceScore: '/weiboCommunicationForceScore', // 微博传播力分数
+  PropagationPath: '/bigscreenPopularArticlesPropagationPath' // 微博路径
 }
